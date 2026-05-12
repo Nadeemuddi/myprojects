2be8d7f9be88cd1d2ab97fcad08e45ca44039bf9
nadeemuddin <nadeemoddin2299@gmail.com>
2026-05-12 10:53:34 +0530

    fixing route

--- routes/listing.js
+++ routes/listing.js
@@ -13,8 +13,8 @@ router.route("/")
   .get(wrapAsync(listingController.index))
   .post(
     isLoggedIn,
-    validateListing,
     upload.single("listing[image][url]"),
+    validateListing,
     wrapAsync(listingController.createListing)
   );
 
